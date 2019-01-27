74f0942e510d13aeec89b4efc5343d96afaaddb7
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-27 20:38:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
  strings = array.unshift.join(" , ")
  new_array = [ ]
  new_array = array.pop
- final = strings.to_a << new_array
+ final = strings.split << new_array
  new_string = final.join(" and ")
 end
