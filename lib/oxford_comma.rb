c98fb819036ba610ce05fb34faa88e70db67a8fb
Sandesh Karki <karkisandesh@hotmail.com>
2019-01-27 20:30:43 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,6 @@ def oxford_comma(array)
   new_array = [ ]
   new_array = array.pop
   whole = [ ]
-  whole = array_length + new_array
+  whole = array_length << new_array
   string = whole.join(" and ")
 end
